25ad6a539ed37d4806f482cf234f2e35923062a9
hakimks <hakimks@gmail.com>
2021-09-11 12:51:21 +0300

    fix

---
+++ origin_set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=kubsCtl7ksdgvjjs3netsa
+export POSTGRES_HOST=kubernetsdatabasedev.cxsjk9tyqhgr.us-east-2.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=arn:aws:s3:::udagram-226795172869-dev
+export AWS_REGION=us-east-1
+export AWS_PROFILE=admin
+export JWT_SECRET=Hello
+export URL=http://localhost:8100
