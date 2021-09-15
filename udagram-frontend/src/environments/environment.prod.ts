1c74b76c1f30e2e0336a9b302f7ae56972d472a3
hakimks <hakimks@gmail.com>
2021-09-15 20:48:36 +0300

    fix ngix and change the ip address

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a8bb216d60dd547d39739cf3a20a4675-25178359.us-east-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
